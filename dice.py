b6b5ecd20389c1a281c5612a39f78d3356101a1a
ziyiligrace <69704067+ziyiligrace@users.noreply.github.com>
2020-09-13 19:49:36 -0700

    Update dice.py

--- dice.py
+++ dice.py
@@ -2,10 +2,10 @@ from random import choices
 ntrials=15000
 player1wins=0
 for i in range(ntrials) :
-    player2=choices(range(1,6) ,k=2)
+    player2=choices(range(1,7) ,k=2)
     if player2[0] == player2[1]:
         continue
-    player1=choices(range(1,6) ,k=3)
+    player1=choices(range(1,7) ,k=3)
     player1.sort(reverse=True)
     if player1[0] + player1[1] > player2[0] + player2[1]:
         player1wins +=1
